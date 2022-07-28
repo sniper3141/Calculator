8a656eafb845cc14bd93db0b65dea24ec6c9b233
James <james.hartman0522@gmail.com>
2022-07-28 11:23:02 +0100

    added header

---
+++ final_project_react/src/Containers/CalcBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Components/Header";
+
+const CalcBox = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+        
+    )
+}
+
+export default CalcBox;
